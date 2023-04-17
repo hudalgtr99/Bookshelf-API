cba64ad729a2be1b1db9fbffe85b406661ab3b4b
Miftahul Huda Guntara <hudalgtr99@gmail.com>
2023-04-17 23:02:56 +0700

    Submission 1.0

---
+++ src/routes.js
@@ -0,0 +1,43 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+    options: {
+      description: 'Menampilkan seluruh buku yang mengandung nama, yang sedang dibaca, dan sudah selesai dibaca.',
+      notes: 'Query params ?name, ?reading, dan ?finished',
+      tags: ['api'],
+    },
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+
+];
+
+module.exports = routes;
